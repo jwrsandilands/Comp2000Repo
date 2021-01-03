373bcb326c6774a689925b9b39f298271094a4b5
Joseph S <55706784+jwrsandilands@users.noreply.github.com>
2021-01-03 12:59:48 +0000

    Working on Class Diagram
    Commit of the beginnings of my class diagram, which I am trying to plan out best I can.

---
+++ Comp2000CWProj/src/AdminView.java
@@ -0,0 +1,8 @@
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("bc07adaa-e072-453b-9d1d-60d58dd4d230")
+public abstract class AdminView {
+    @objid ("7fd8ac85-5209-4465-b079-01bd0a9c06b9")
+    public IAdminView iAdminView;
+
+}
